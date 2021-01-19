55a1c776edfca686ce554ca8e747fcb7c380b848
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-01-19 14:35:23 -0500

    smth broken with everyone

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const { resolveSoa } = require('dns');
 const express = require('express'); // like a PHP require
 const server = express();
+const path = require('path');
+
+
+// set our views directory
+server.set("views", path.join(__dirname, 'views'));
+
+// set the static assets director so Express knows where to look
+//
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.get("/", (req, res) => {
   console.log('hit the home route');
-  res.send('your first express app');
+  // res.send('your first express app');
+  res.sendFile('views/index.html');
 })
 
 server.get("/contact", (req, res) => {
   console.log('hit the contact route');
-  res.send('contact page');
+  // res.send('contact page');
+  res.sendFile('views/contact.html');
 })
 
 // require is more or less the same as a JS import
